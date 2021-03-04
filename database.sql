dfa7790cd86494aebeba674f9e50a172664ac0e4
Prayogaea <70587998+Prayogaea@users.noreply.github.com>
2021-03-04 19:37:26 +0700

    upload
    upload

---
+++ database.sql
@@ -0,0 +1,13 @@
+
+database = "login_lengkap";
+
+
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+`userID` int(11) NOT NULL AUTO_INCREMENT,
+`userName` varchar(100) NOT NULL,
+`userEmail` varchar(100) NOT NULL UNIQUE,
+`userPass` varchar(100) NOT NULL,
+`userStatus` enum('Y','N') NOT NULL DEFAULT 'N',
+`tokenCode` varchar(100) NOT NULL,
+PRIMARY KEY (`userID`)
+)
